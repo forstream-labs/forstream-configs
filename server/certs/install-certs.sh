538ed5f0a5ecab40d1945ce88606b0bc1cafeff1
Sandro Simas <sandro.dacosta@uala.com.ar>
2020-05-27 19:48:40 -0300

    Improving scripts

--- server/certs/install-certs.sh
+++ server/certs/install-certs.sh
@@ -7,14 +7,14 @@ if [ -z "$FORSTREAM_HOME" ]; then
   exit 1
 fi
 
-# echo "Installing forstream certificate..."
-# sudo letsencrypt certonly --standalone -d forstream
+echo "Installing forstream.io certificate..."
+sudo letsencrypt certonly --standalone -d forstream.io
 
-echo "Installing api.forstream certificate..."
-sudo letsencrypt certonly --standalone -d api.forstream
+echo "Installing www.forstream.io certificate..."
+sudo letsencrypt certonly --standalone -d www.forstream.io
 
-# echo "Installing www.forstream certificate..."
-# sudo letsencrypt certonly --standalone -d www.forstream
+echo "Installing api.forstream.io certificate..."
+sudo letsencrypt certonly --standalone -d api.forstream.io
 
 echo ""
 echo "Certificates installed with success!"
