8c15c8dea1e32edacf60df6638acc89889184203
Sandro Simas <sandro.dacosta@uala.com.ar>
2020-05-28 00:01:33 -0300

    Improving scripts

--- server/docker/create-network.sh
+++ server/docker/create-network.sh
@@ -5,12 +5,7 @@ set -e
 echo "Creating Docker network forstream..."
 echo ""
 
-docker network create \
-  --driver bridge \
-  --subnet 172.30.0.0/16 \
-  --ip-range 172.30.5.0/24 \
-  --gateway 172.30.5.254 \
-  forstream
+docker network create --driver bridge forstream
 
 echo ""
 echo "Docker network forstream was created with success!"
