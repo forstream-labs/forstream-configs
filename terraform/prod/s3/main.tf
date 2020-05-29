9c332d1d6c7fbdebb7eb27142420a50cf5b99a02
Sandro Simas <sandro.dacosta@uala.com.ar>
2020-05-28 23:51:39 -0300

    Updating acl of public bucket to private

--- terraform/prod/s3/main.tf
+++ terraform/prod/s3/main.tf
@@ -3,7 +3,7 @@ module "forstream_public_s3_bucket" {
 
   environment = var.environment
   bucket = "forstream-public"
-  acl = "public-read"
+  acl = "private"
 }
 
 module "forstream_private_s3_bucket" {
